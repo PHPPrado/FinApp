9df54199a47807cc6187b88eca6ccae3aaf3d68e
Pedro <pedro.phprado@gmail.com>
2023-12-19 14:24:56 -0300

    fix: corrigindo bug de projeto

--- src/main/java/com/fin/fin/domain/renda/RendaRepository.java
+++
@@ -1,6 +0,0 @@
-package com.fin.fin.domain.renda;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RendaRepository extends JpaRepository<Renda, Integer> {
-}
--- src/main/java/com/fin/fin/domain/usuario/UsuarioRepository.java
+++
@@ -1,6 +0,0 @@
-package com.fin.fin.domain.usuario;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
-}
